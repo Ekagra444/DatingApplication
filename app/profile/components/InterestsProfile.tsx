8749e6ee1ccebc2b4ba3bbf4ae937d6f3b9f32b0
Shivam-Jangid <shivamjangid1125@gmail.com>
2025-07-15 17:44:19 +0530

    profile ready

---
+++ app/profile/components/InterestsProfile.tsx
@@ -0,0 +1,79 @@
+import { BookOpen, Camera, ChefHat, Coffee, Dog, Dumbbell, Heart, Mountain, Music, Palette, Plane, Wine } from "lucide-react";
+
+const defaultInterests = [
+  'Photography',
+  'Hiking', 
+  'Cooking',
+  'Travel',
+  'Yoga',
+  'Music',
+  'Art',
+  'Reading',
+  'Wine Tasting',
+  'Dogs'
+];
+interface InterestsDisplayProps {
+  interests?: string[]
+  lifestyle?: {
+    exercise?: string
+    pets?: string
+    lifestyle?: string
+  }
+}
+
+const lifestyleIcons = {
+  exercise: Dumbbell,
+  pets: Dog,
+  lifestyle: Coffee
+}
+const defaultLifestyle = {
+  exercise: 'Regular Exercise',
+  pets: 'Dog Lover',
+  lifestyle: 'Coffee Enthusiast'
+}
+const interestIcons = {
+  'Photography': Camera,
+  'Hiking': Mountain,
+  'Cooking': ChefHat,
+  'Travel': Plane,
+  'Yoga': Heart,
+  'Music': Music,
+  'Art': Palette,
+  'Reading': BookOpen,
+  'Wine Tasting': Wine,
+  'Dogs': Dog
+}
+
+export default function InterestsProfile({interests = defaultInterests, lifestyle = defaultLifestyle}:InterestsDisplayProps) {
+
+  return (
+    <div className="min-h-screen px-10 py-4 w-full space-y-12">
+        <div className="flex justify-center mt-10 text-3xl font-bold">Interests</div>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 max-w-5xl mx-auto">
+            {interests.map((interest, index) => {
+                const IconComponent = interestIcons[interest as keyof typeof interestIcons];
+                return <div key={index} className="flex flex-col items-center gap-3 p-6 rounded-xl border hover:shadow-lg transition-all duration-200 border-[#2f2f2f] hover:bg-[#2f0d0d]/70 cursor-pointer bg-[#1a1a1a] group">
+                        <div className="w-12 h-12 transition-all duration-200 bg-[#c25c5c]/30 group-hover:bg-[#741e1e] rounded-full flex items-center justify-center">
+                            {IconComponent && <IconComponent className="w-6 h-6 text-[#de4d4d]" />}
+                        </div>
+                        <span className="font-medium text-center">{interest}</span>
+                </div>
+            })}
+        </div>
+        <div className="flex justify-center text-3xl font-bold">Lifestyle</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
+            {Object.entries(lifestyle).map(([key, val])=>{
+                const IconComponent = lifestyleIcons[key as keyof typeof lifestyleIcons]
+                return <div key={key} className="flex items-center gap-4 p-6 bg-[#1a1a1a] rounded-xl border border-[#2f2f2f] hover:shadow-lg transition-all hover:bg-[#2f0d0d]/70 duration-200 cursor-pointer group">
+                    {IconComponent && (
+                                <div className="flex-shrink-0 w-14 h-14 rounded-full flex items-center justify-center group-hover:bg-[#741e1e] transition-colors bg-[#c25c5c]/30">
+                                    <IconComponent className="w-7 h-7 text-[#de4d4d]" />
+                                </div>
+                    )}
+                    <span className="text font-semibold text-lg">{val}</span>
+                </div>
+            })}
+        </div>
+    </div>
+  )
+}
--- app/profile/components/Sidebar.tsx
+++ app/profile/components/Sidebar.tsx
@@ -1,15 +1,105 @@
-import BestMatchsIcon from "./Sidebarcomponents/Icons/BestMatchsIcon";
-import MessagesIcon from "./Sidebarcomponents/Icons/MessagesIcon";
-import SettingIcon from "./Sidebarcomponents/Icons/SettingIcon";
-import UserIcon from "./Sidebarcomponents/Icons/UserIcon";
-import Items from "./Sidebarcomponents/Items";
-export default function Sidebar() {
+import { Heart, HelpCircle, MessageCircle, Search, Settings, User, X } from "lucide-react";
+interface sidebarProps{
+  isOnline:boolean;
+  setisOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface navItemProps{
+  label:string;
+  icon: React.ReactNode;
+  href: string;
+}
+
+const navigationItems: navItemProps[] = [
+  {
+    label: "Discover",
+    icon: <Search className="h-5 w-5 transition-all duration-200"/>,
+    href: "/discover" 
+  },
+  {
+    label: "Matches",
+    icon: <Heart className="h-5 w-5 transition-all duration-200" />,
+    href: "/matches"
+  },
+  {
+    label: "Messages",
+    icon: <MessageCircle className="h-5 w-5 transition-all duration-200 " />,
+    href: "/messages"
+  },
+  {
+    label: "Profile",
+    icon: <User className="h-5 w-5 transition-all duration-200" />,
+    href: "/profile"
+  },
+  {
+    label: "Settings",
+    icon: <Settings className="h-5 w-5 transition-all duration-200" />,
+    href: "/settings"
+  },
+  {
+    label: "Help & Support",
+    icon: <HelpCircle className="h-5 w-5 transition-all duration-200 " />,
+    href: "/help"
+  }
+];
+
+
+export default function Sidebar(props:sidebarProps) {
+  function closeSideBar(){
+    props.setisOpen(val => !val);
+}
   return (
-    <div className="h-screen mt-12 flex flex-col w-full items-center gap-3">
-        <Items text="My Profile" StartIcon={<UserIcon size="size-4"/>}/> 
-        <Items text="Messages" StartIcon={<MessagesIcon size="size-3" />}/>        
-        <Items text="Best Matches" StartIcon={<BestMatchsIcon size="size-4" />}/> 
-        <Items text="Settings" StartIcon={<SettingIcon size="size-4"/>}/> 
+    <div className="h-screen w-[300px] bg-gradient-to-b overflow-y-auto overflow-x-hidden from-black to-[#1a1a1a] border-r border-[#262626]/40">
+        <div className="pt-8 flex items-start justify-around">
+          <div className="flex gap-4">
+            <div className="rounded-2xl bg-red-600/10 ring-2 ring-red-500/30 flex items-center px-3.5">
+              <Heart className="size-6 text-red-400"/>
+            </div>
+            <div>
+              <div className="text-2xl font-bold">
+                Etrenite
+              </div>
+              <div className="opacity-40 text-sm">
+                Find your match
+              </div>
+            </div>
+          </div>
+          <div onClick={closeSideBar} className="hover:text-slate-500 cursor-pointer">
+            <X className="size-6"/>
+          </div>
+        </div>
+        <div className=" py-9 flex justify-center">
+          <div className="transition-all duration-200 bg-[#1a1a1a]/50 border border-[#262626]/40 hover:shadow-lg hover:bg-[#262626]/80 p-4 rounded-xl flex items-center gap-4">
+          <div className="bg-red-600/10 flex rounded-full text-red-400 font-semibold ring-2 ring-red-500/40 text-lg px-3 h-full items-center ">
+          SJ
+          </div>
+          <div>
+            <div className="font-semibold tracking-tight">Emma Watson</div>
+            <div className="flex items-center gap-3">
+            <div className={`rounded-full size-2 brightness-125 ${props.isOnline?"bg-green-500":"bg-red-500"}`}>
+            </div>
+            <div className="text-sm opacity-70">{props.isOnline?"Online now": "Currently Offline"}</div>
+          </div>
+          </div> 
+          </div>  
+        </div>
+        <div className="relative">
+          <div className="flex flex-col gap-2 pl-6 mt-5">
+            {navigationItems.map((item, index)=>(
+              <div key={item.label} className="flex items-center gap-4 p-3 rounded-lg hover:text-pink-400 cursor-pointer">
+                <div className="opacity-70">
+                  {item.icon}
+                </div>
+                <div>
+                  {item.label}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
     </div>
   )
+
+  
 }
