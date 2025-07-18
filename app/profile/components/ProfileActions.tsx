8e884d924069267df26943c07e4c0dc5292deb7a
Shivam-Jangid <shivamjangid1125@gmail.com>
2025-07-18 15:31:02 +0530

    added some dark mode features

--- app/profile/components/ProfileActions.tsx
+++ app/profile/components/ProfileActions.tsx
@@ -6,20 +6,20 @@ interface profileActionProps{
 
 export default function ProfileActions({sidebarOpen}:profileActionProps) {
   return (
-    <div className={`fixed bottom-0 w-full flex flex-col z-50 bg-[#1a1a1a]/80 backdrop-blur-md border-t border-[#2f2f2f] shadow-lg items-center ${sidebarOpen &&"-ml-[300px]"}`}>
+    <div className={`fixed bottom-0 w-full flex flex-col z-50 bg-card/80 backdrop-blur-md border-t border-[#2f2f2f]/10 shadow-lg items-center ${sidebarOpen &&"-ml-[300px]"}`}>
         <div>
             {/* primary Action Buttons */}
-            <div className="py-5 flex gap-10">
-                <button className="rounded-full transition-all hover:scale-105 border p-6 border-[#2f2f2f] bg-[#1a1a1a] text-white"><X className="size-4"/></button>
-                <button className="rounded-full transition-all hover:scale-105 border p-6 border-[#2f2f2f] bg-[#1a1a1a] text-white"><Star className="size-4"/></button>
-                <button className="rounded-full transition-all hover:scale-105 border p-6 border-[#2f2f2f] bg-[#f76f6f] text-white"><Heart className="size-4"/> </button>
+            <div className="py-3 flex gap-10">
+                <button className="rounded-full transition-all hover:scale-105 border p-6 border-[#2f2f2f]/30 bg-card "><X className="size-4"/></button>
+                <button className="rounded-full transition-all hover:scale-105 border p-6 border-[#2f2f2f]/30 bg-card "><Star className="size-4"/></button>
+                <button className="rounded-full transition-all hover:scale-105 border p-6 border-[#2f2f2f]/30 bg-[#f76f6f] "><Heart className="size-4"/> </button>
             </div>
             
         </div>
         <div className="flex gap-4 mb-5">
                 {/* Secondary Action Buttons  */}
-                <button className="flex gap-2 bg-[#1a1a1a] transition-all hover:scale-[1.02] border rounded-3xl px-20 py-3 border-[#2f2f2f]">🌹<span className="text-pink-600 ">Send Rose</span></button>
-                <button className="flex gap-2 items-center bg-white transition-all hover:scale-[1.02] border rounded-3xl px-20 py-3 border-[#2f2f2f] hover:text-red-600"><MessageCircleMore className="size-5 text-black/80"/><span className="text-black transition-all">Message</span></button>
+                <button className="flex gap-2 bg-card transition-all hover:scale-[1.02] border rounded-3xl px-20 py-3 border-[#2f2f2f]/40">🌹<span className="text-pink-600 ">Send Rose</span></button>
+                <button className="flex gap-2 items-center bg-pink-700/80 dark:bg-white text-background transition-all hover:scale-[1.02] border rounded-3xl px-20 py-3 border-[#2f2f2f]/40"><MessageCircleMore className="size-5 text-background"/><span className="transition-all">Message</span></button>
             </div>
     </div>
   )
